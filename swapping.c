e0410f6b14746a1b9af1f4381c33000aaf8a167e
Andrewjeffri <104091949+Andrewjeffri@users.noreply.github.com>
2022-04-20 23:13:12 +0530

    Update swapping.c

--- swapping.c
+++ swapping.c
@@ -4,7 +4,7 @@ main()
   int a,b;
   printf("enter a,b values : ");
   scanf("%d%d",&a,&b);
-  printf("Before swapping a=%d,b=%d",a,b);
+  printf("Before swapping a=%d,b=%d \n",a,b);
   a=a+b;
   b=a-b;
   a=a-b;
