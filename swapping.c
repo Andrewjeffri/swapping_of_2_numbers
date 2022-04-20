1e776abf2b62633c8f2b73fafe03725edb60b430
Andrewjeffri <104091949+Andrewjeffri@users.noreply.github.com>
2022-04-20 23:06:07 +0530

    created swapping.c
    The file is used swap the 2 numbers

---
+++ swapping.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+  int a,b;
+  printf("enter a,b values : ");
+  scanf("%d%d",&a,&b);
+  printf("Before swapping a=%d,b=%d",a,b);
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  print("After swapping a=%d,b=%d",a,b);
+}
